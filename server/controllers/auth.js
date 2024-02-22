abf91a8566bd4a692c6ce6f04cfcd107e6ffae1f
minhtuan1611 <tuan.mn51@gmail.com>
2024-02-22 09:27:55 +0200

    fix authenticaion

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -1,8 +1,8 @@
 import bcrypt from 'bcrypt'
 import jwt from 'jsonwebtoken'
 import User from '../models/User.js'
-import { response } from 'express'
 
+/* REGISTER USER */
 export const register = async (req, res) => {
   try {
     const {
@@ -33,27 +33,25 @@ export const register = async (req, res) => {
     })
     const savedUser = await newUser.save()
     res.status(201).json(savedUser)
-  } catch (error) {
+  } catch (err) {
     res.status(500).json({ error: err.message })
   }
 }
 
-// loggin in
-
+/* LOGGING IN */
 export const login = async (req, res) => {
   try {
     const { email, password } = req.body
     const user = await User.findOne({ email: email })
-    if (!user) return res.status(400).json({ msg: "User doesn't exist. " })
+    if (!user) return res.status(400).json({ msg: 'User does not exist. ' })
 
     const isMatch = await bcrypt.compare(password, user.password)
-    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials.' })
+    if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials. ' })
 
     const token = jwt.sign({ id: user._id }, process.env.JWT_SECRET)
     delete user.password
-
-    res.status(200), json({ token, user })
-  } catch (error) {
+    res.status(200).json({ token, user })
+  } catch (err) {
     res.status(500).json({ error: err.message })
   }
 }
