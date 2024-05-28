f11e36a8b871151eb98df57320b3dc04b6b8b545
minhtuan1611 <tuan.mn51@gmail.com>
2024-05-29 02:26:08 +0300

    change css style

--- client/src/scenes/messagePage/index.jsx
+++ client/src/scenes/messagePage/index.jsx
@@ -1,19 +1,30 @@
-import MessageContainer from '../../components/messages/MessageContainer'
 import { Box } from '@mui/material'
 import Sidebar from '../../components/sidebar/Sidebar'
+import MessageContainer from '../../components/messages/MessageContainer'
 import Navbar from 'scenes/navbar'
+import { useSelector } from 'react-redux'
+import { CssBaseline, ThemeProvider } from '@mui/material'
+import { createTheme } from '@mui/material/styles'
+import { themeSettings } from 'theme'
 
 const Home = () => {
+  const mode = useSelector((state) => state.mode)
+  const theme = createTheme(themeSettings(mode))
+
   return (
-    <Box>
-      <Navbar />
-      <div className="chat-app">
-        <div className="container ">
-          <Sidebar />
-          <MessageContainer />
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      <Box>
+        <Navbar />
+        <div className="chat-app">
+          <div className="container">
+            <Sidebar />
+            <MessageContainer />
+          </div>
         </div>
-      </div>
-    </Box>
+      </Box>
+    </ThemeProvider>
   )
 }
+
 export default Home
