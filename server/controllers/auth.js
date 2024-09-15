8f786ca5d203d304c73592af49ae6f6ee381ec86
Nguyễn Minh Tuấn <nguyenminhtuan@MacBook-Pro-cua-Nguyen.local>
2024-09-15 14:58:42 +0300

    fix: syntax

--- server/controllers/auth.js
+++ server/controllers/auth.js
@@ -47,9 +47,9 @@ export const login = async (req, res) => {
     if (!isMatch) return res.status(400).json({ msg: 'Invalid credentials. ' })
 
     const token = jwt.sign(
-      { expiresIn: 3 * 24 * 60 * 60 }, // 3 days
       { id: user._id },
-      process.env.JWT_SECRET
+      process.env.JWT_SECRET,
+      { expiresIn: 3 * 24 * 60 * 60 } // 3 days
     )
     delete user.password
     res.status(200).json({ token, user })
